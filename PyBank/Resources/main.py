e264f04379812c8defd518b0a4ba970df0ba1eea
Kevin-Lad <kevinladisheff@gmail.com>
2023-06-29 13:31:20 -0700

    average change complete

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -14,7 +14,35 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
+    total_profit_losses = 0
+    total_months = 0 
+    sum_profit_losses = 0 
+
     for row in csvreader:
         
-        profit_losses = row[1]
-        print(profit_losses)
+        profit_losses = int(row[1])
+        #print(profit_losses)
+
+        total_profit_losses = total_profit_losses + profit_losses
+        total_months = total_months + 1
+        
+        if total_months > 1:
+            change = profit_losses - last_profit_losses
+            sum_profit_losses = sum_profit_losses + change
+
+        last_profit_losses = int(row[1])
+       
+
+    average_change = (sum_profit_losses / (total_months -1))
+    #print(total_profit_losses)
+    #print(total_months)
+    #print (average_change)
+
+    # print output
+    print('Financial Anlaysis')
+    print('----------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Total: ${total_profit_losses}')
+    print(f'Average Change: ${average_change}')
+    print('Greatest increase in profits: ')
+    print('Greatest decrease in profits: ')
