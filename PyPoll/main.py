fbc84c9b069b30633a8537b97d67fafacf5e2bd7
Kevin-Lad <kevinladisheff@gmail.com>
2023-07-08 13:35:54 -0700

    final update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,12 +11,10 @@ with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    #print(csvreader)
-
+   
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-
+ 
     charles_votes = 0
     charles = "Charles Casper Stockham"
     diana_votes = 0
@@ -24,6 +22,7 @@ with open(csvpath) as csvfile:
     raymon_votes =0
     raymon = "Raymon Anthony Doane"
 
+    # This for loop is where all the calculations will be done
     for row in csvreader:
 
         if charles == row[2]:
